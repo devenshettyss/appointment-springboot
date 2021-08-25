fe56a6a4a7914410108f337d08ff456c6674c273
devenshettyss <practice.devenshetty@gmail.com>
2021-08-25 12:42:20 +0000

    Added validation along with request mappings

--- src/main/java/com/tcs/appointment/appointment/AppointmentController.java
+++ src/main/java/com/tcs/appointment/appointment/AppointmentController.java
@@ -3,12 +3,26 @@ package com.tcs.appointment.appointment;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+
 
 @RestController
 public class AppointmentController {
@@ -29,6 +43,34 @@ public class AppointmentController {
 	@GetMapping("getAppointment/{id}")
 	private Optional<Appointment> getAppointmentById(@PathVariable int id) {
 		return appointmentRepository.findById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteAppointment(@PathVariable("id") Integer id) {
+		appointmentRepository.deleteById(id);
+	}
+	
+	@PutMapping("/update")
+	private void updateAppointment(@RequestBody Appointment appointment) {
+		appointmentRepository.save(appointment);
+		System.out.println(appointment.getUser_name());
+	}
+	
+	@ExceptionHandler(value = {AppointmentNotFoundException.class,EmptyResultDataAccessException.class})
+	public ResponseEntity<Appointment> exception(AppointmentNotFoundException AppointmentNotFoundException) {
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	@PostMapping("/valid")
+	public void saveuser(@Valid @RequestBody Appointment appointment) {
+
+		if (appointment.getUser_name() == null || appointment.getUser_name().length() <= 5) {
+			throw new RuntimeException("Invalid Entry!!"); 
+			
+		}
+
+		appointmentRepository.save(appointment);
+
 	}
 
 
---
+++ src/main/java/com/tcs/appointment/appointment/AppointmentNotFoundException.java
@@ -0,0 +1,11 @@
+package com.tcs.appointment.appointment;
+
+public class AppointmentNotFoundException extends RuntimeException {
+
+	public AppointmentNotFoundException(String message) {
+		super(message);
+		
+	}
+	
+
+}
